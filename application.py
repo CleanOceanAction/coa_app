e0d25f9ac34d7a8c8073f45c6effaa3bf0a4ca0b
User <user@R-n-D-MacBook-Pro.local>
2015-09-06 09:31:20 -0400

    changing app structure

--- application.py
+++ application.py
@@ -12,6 +12,8 @@ Bind to PORT if defined, otherwise default to 8080
 
 from beach import application
 
+
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 8080
-    application.run(host='0.0.0.0', port=application.config['PORT'], debug=application.config['DEBUG'])
+    application.run(host='0.0.0.0',
+                    port=application.config['PORT'],
+                    debug=application.config['DEBUG'])
